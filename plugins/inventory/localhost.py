fe9ee3de37f04fdb9f2caaeb10191adf40e35eae
Roman Khmelichek <roman.khmelichek@gmail.com>
2022-04-09 14:41:33 -0400

    Inventory plugin improvements.

--- plugins/inventory/localhost.py
+++ plugins/inventory/localhost.py
@@ -4,8 +4,14 @@ __metaclass__ = type
 DOCUMENTATION = r'''
     name: localhost
     plugin_type: inventory
+    options:
+        plugin:
+            description: Plugin name
+            required: true
+            choices: ['rkhmelichek.developer.localhost']
 '''
 
+import os
 import sys
 
 from ansible.plugins.inventory import BaseInventoryPlugin
@@ -13,15 +19,18 @@ from ansible.plugins.inventory import BaseInventoryPlugin
 class InventoryModule(BaseInventoryPlugin):
     NAME = 'localhost'
 
+    def __init__(self):
+        super(InventoryModule, self).__init__()
+
     def verify_file(self, path):
         valid = False
         if super(InventoryModule, self).verify_file(path):
-            if path.endswith(('localhost.yml')):
+            if os.path.basename(path) == 'localhost.yml':
                 valid = True
         return valid
 
-    def parse(self, inventory, loader, path, cache=True):
-        super(InventoryModule, self).parse(inventory, loader, path)
+    def parse(self, inventory, loader, path, cache=False):
+        super(InventoryModule, self).parse(inventory, loader, path, cache=cache)
 
         self.inventory.add_host('localhost')
         self.inventory.set_variable('localhost', 'ansible_connection', 'local')
